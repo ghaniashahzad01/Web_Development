4a573151c46b17055c3fde7277ba3c9e73497411
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-01-23 22:39:37 +0500

    Implement switch statement in Javascript.

---
+++ Javascript/switch_statement.js
@@ -0,0 +1,30 @@
+let day = 3;
+
+switch(day){
+    
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+     case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thursday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Saturday");
+        break;
+    case 7: 
+       console.log("Today is Sunday");
+        break;  
+    default:
+        console.log("Invalid Day");
+        break;  // This will be executed if no case matches the given value.
+
+    }
