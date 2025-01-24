ad79ce0b7b54a0d7e8a9548f4a4304fd696774ad
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-01-24 23:02:14 +0500

    Implement loops in Javacript.

---
+++ loops.js
@@ -0,0 +1,19 @@
+let sum = 0;
+
+for (let i = 1; i <= 5; i++) {
+  sum += i; // Add the current value of i to sum
+}
+
+console.log("The sum of the first 5 numbers is:", sum);
+
+
+let i = 1;
+let sum = 0;
+
+while (i <= 20) {
+  sum += i; // Add the current value of i to sum
+  i++; // Increment i after each iteration
+}
+
+console.log("The sum of numbers from 1 to 20 is:", sum);
+
