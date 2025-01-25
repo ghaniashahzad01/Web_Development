cdee23c11033740a8be61c201bc1447b969ebf3f
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-01-25 21:25:49 +0500

    Convert Boolean value as a string.

--- Javascript/conversions.js
+++ Javascript/conversions.js
@@ -1,8 +1,26 @@
 "use strict";
 
-let names = "Ghania"
+// let names = "Ghania"
 
-console.log(typeof names);
+// console.log(typeof names);
 
-let nam = Number(names);
-console.log(nam);
+// let nam = Number(names);
+// console.log(nam);
+
+// let age = "34"
+
+// console.log(typeof age);
+
+// let ages = Number(age);
+
+// console.log(typeof ages);
+
+let isLogged = false;
+
+console.log(typeof isLogged);
+
+let LoggedIn = String(isLogged);
+
+console.log(typeof LoggedIn);
+
+console.log(LoggedIn);
