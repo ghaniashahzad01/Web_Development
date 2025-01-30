4262b21132d21d98baf0c147256c82dc323bd297
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-01-30 15:42:14 +0500

    Create an object in js.

---
+++ Javascript/Objects.js
@@ -0,0 +1,11 @@
+// Creating an object
+let student = {
+    firstName: "Ghania",
+    lastName: "Shahzad",
+    age: 22,
+    greet: function() {
+      return `Hello, my name is ${this.firstName} ${this.lastName}.`;
+    }
+  };
+  
+  
