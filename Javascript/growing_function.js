221bb1f34a150d2c9b8c17de8ed9b1ca1c091e6c
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-02-04 22:29:38 +0500

    Implement growing function.

--- Javascript/growing_function.js
+++ Javascript/growing_function.js
@@ -5,3 +5,12 @@ function zeroPad(number, width) {
     }
     return string;
   }
+
+  function printFarmInventory(cows, chickens, pigs) {
+    console.log(`${zeroPad(cows, 3)} Cows`);
+    console.log(`${zeroPad(chickens, 3)} Chickens`);
+    console.log(`${zeroPad(pigs, 3)} Pigs`);
+  }
+  
+  printFarmInventory(7, 16, 3);
+  
