a16db4b189dfd5487a7f5ec9a12f24b4204b8f30
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-02-05 23:29:23 +0500

    Added objects.

--- Javascript/Object.js
+++ Javascript/Object.js
@@ -4,5 +4,20 @@ let journal = [
     { events: ["weekend", "cycling", "peanuts"], squirrel: true }
   ];
   
-  
+// Accessing and modifying journal entries
+console.log(journal[0].squirrel);  // false
+journal[0].wolf = false;           // Adding a new property
+delete journal[0].wolf;            // Removing a property
+
+// Checking properties
+console.log("squirrel" in journal[0]);  // true
+
+// Copying properties with Object.assign()
+Object.assign(journal[1], { mood: "happy" });
+console.log(journal[1]);
+
+// Adding a new entry
+journal.push({ events: ["work", "dinner"], squirrel: true });
+
+console.log(journal);
   
