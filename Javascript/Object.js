31d42dbf4a3ebe7be839938667a9dd5c56c2dce6
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-02-05 23:28:10 +0500

    Implement objects in js.

---
+++ Javascript/Object.js
@@ -0,0 +1,8 @@
+let journal = [
+    { events: ["work", "touched tree", "pizza"], squirrel: false },
+    { events: ["work", "ice cream", "cauliflower"], squirrel: false },
+    { events: ["weekend", "cycling", "peanuts"], squirrel: true }
+  ];
+  
+  
+  
