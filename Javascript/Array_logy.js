99b9ebb5533bee4cc4c3aa5df2e8a6d2f003b96e
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-02-07 23:34:32 +0500

    Implement Array logy.

--- Javascript/Array_logy.js
+++ Javascript/Array_logy.js
@@ -22,3 +22,14 @@ let array = [0, 1, 2, 3, 4];
 console.log(array.slice(2, 4));  // → [2, 3]
 console.log(array.slice(2));     // → [2, 3, 4]
 
+// Demonstrating concat
+let letters = ["a", "b", "c"];
+console.log(letters.concat(["d", "e"])); // → ["a", "b", "c", "d", "e"]
+console.log(letters.concat("f"));        // → ["a", "b", "c", "f"]
+
+// Function to remove an element at a given index
+function remove(array, index) {
+  return array.slice(0, index).concat(array.slice(index + 1));
+}
+
+console.log(remove(["a", "b", "c", "d", "e"], 2)); // → ["a", "b", "d", "e"]
