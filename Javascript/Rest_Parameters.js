b77e713c168032e579a93c7efa8fc6cf9977c16d
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-02-10 23:15:13 +0500

    Add some changes.

--- Javascript/Rest_Parameters.js
+++ Javascript/Rest_Parameters.js
@@ -1,18 +1,12 @@
 function max(...numbers) {  
-    let result = -Infinity;  
-    for (let number of numbers) {  
-      if (number > result) result = number;  
-    }  
-    return result;  
+  let result = -Infinity;  
+  for (let number of numbers) {  
+    if (number > result) result = number;  
   }  
-  console.log(max(4, 1, 9, -2)); // Output: 9  
+  return result;  
+}  
+console.log(max(4, 1, 9, -2)); // Output: 9  
 
- let nums = [5, 1, 7];  
+let nums = [5, 1, 7];  
 console.log(max(...nums)); // Output: 7  
 
-
-let words = ["never", "fully"];  
-console.log(["will", ...words, "understand"]); // Output: ["will", "never", "fully", "understand"]  
-
-let coordinates = { x: 10, y: 0 };  
-console.log({ ...coordinates, y: 5, z: 1 }); // Output: { x: 10, y: 5, z: 1 }  
