e440970b7b4459cc2785f34f209036e92400beb4
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-02-10 23:15:42 +0500

    Final commit.

--- Javascript/Rest_Parameters.js
+++ Javascript/Rest_Parameters.js
@@ -10,3 +10,9 @@ console.log(max(4, 1, 9, -2)); // Output: 9
 let nums = [5, 1, 7];  
 console.log(max(...nums)); // Output: 7  
 
+
+let words = ["never", "fully"];  
+console.log(["will", ...words, "understand"]); // Output: ["will", "never", "fully", "understand"]  
+
+let coordinates = { x: 10, y: 0 };  
+console.log({ ...coordinates, y: 5, z: 1 }); // Output: { x: 10, y: 5, z: 1 }  
