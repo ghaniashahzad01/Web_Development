9e7221fd81ad144bedd03cee3625a740a1635a21
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-03-21 23:27:31 +0500

    Initial commit.

--- Javascript/Project_12/promise.js
+++ Javascript/Project_12/promise.js
@@ -5,8 +5,30 @@ const promiseOne = new Promise(function(resolve, reject) {
    
     })
 
-promise.then(function(){
+promiseOne.then(function(){
     console.log("First promise created");
     
 })
 
+
+const PromiseTwo = new Promise(function(resolve, reject){
+    let success = true;
+
+     if(success){
+       resolve("Promise is created successfully");
+     }
+
+     else{
+        reject("Something went wrong");
+     }
+});
+
+
+PromiseTwo.then(function(message){
+    console.log(message);
+    
+})
+
+PromiseTwo.catch(function(error){
+    console.log(error);
+})
